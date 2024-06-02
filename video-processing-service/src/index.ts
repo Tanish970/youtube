91bcd38a6d28367e10d18348a9a35bf0d5aecc8e
Tanish970 <rohith.guntur@gmail.com>
2024-06-02 21:12:17 +0000

    faw

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -11,7 +11,6 @@ import {
 } from "./storage";
 
 const app = express();
-<<<<<<< HEAD
 app.use(express.json());
 
 setupDirectories();
@@ -31,31 +30,6 @@ app.post("/process-video", async (req, res) => {
     console.error(error);
     return res.status(400).json({ message: "Bad Request: missing filename." });
   }
-=======
-const port=process.env.port||3000;
-
-
-app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send("Working")
-})
-
-app.post('/process-video', (req, res) => {
-    const inputFilePath=req.body.inputFilePath;
-    const outputFilePath=req.body.outputFilePath;
-    console.log(inputFilePath);
-    console.log(outputFilePath);
-    if (!inputFilePath || !outputFilePath){
-    res.status(400).send('Bad request:Missing File')}
-    ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360')
-    .on('end', () => res.status(200).send("conversion finished"))
-    .on('error', err => {
-        console.log('error: ', err.message)
-        res.status(500).send('Internal Server Error:'+err.message)})
-    .save(outputFilePath)
-    .run();
->>>>>>> refs/remotes/origin/main
 
   const inputFileName = data.name;
   const outputFileName = `processed-${inputFileName}`;
@@ -84,16 +58,8 @@ app.post('/process-video', (req, res) => {
 
   return res.status(200).json({ message: "Processing resolved successfully" });
 });
-<<<<<<< HEAD
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-=======
-
-
-    
-
-app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
->>>>>>> refs/remotes/origin/main
